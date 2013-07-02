18767ea1dc70fc0f12cbce611e4eed757c36e984
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-07-02 08:44:19 +0200

    Added setup.txt to contain what must be set up for this to work. Added first source skeleton

---
+++ src/erlslim_app.erl
@@ -0,0 +1,16 @@
+-module(erlslim_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    erlslim_sup:start_link().
+
+stop(_State) ->
+    ok.
