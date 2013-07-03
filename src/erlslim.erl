032938f8a26822b2cdca06454a0f9f543bd30692
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-07-03 13:28:53 +0200

    Decoder rewritten using old unit tests

--- src/erlslim.erl
+++ src/erlslim.erl
@@ -2,18 +2,21 @@
 -export([start/0]).
 -define(VERSION,"0.3").
 
-start() ->
+start() ->    
     {ok, [[Port]]} = init:get_argument('slim_port'),
     {ok, LSock} = gen_tcp:listen(list_to_integer(Port),[{active,false},{reuseaddr,true}]),
     {ok, ASock} = gen_tcp:accept(LSock),
     gen_tcp:send(ASock,"Slim -- V"++?VERSION++"\n"),
     {ok, Data} = gen_tcp:recv(ASock, 0),
+    io:format("Received:~p~n",[Data]),
     handle_command(ASock, Data),
     gen_tcp:close(ASock),
     gen_tcp:close(LSock),
     exit(0).
 
 handle_command(_, "000003:bye") ->
-    io:format("BYE~n",[]).
-			      
-    
+    io:format("BYE~n",[]);
+handle_command(ASock, Input) ->
+    io:format("command: ~p ~n",[erlslim_decoder:decode(Input)]),
+    gen_tcp:send(ASock,"000020:[000001:000002:OK:]").
+
--- src/erlslim_decoder.erl
+++ src/erlslim_decoder.erl
@@ -5,23 +5,26 @@
 decode(Input) ->
     to_command(decode_2(Input)).
 
-to_command([_,"make","scriptTableActor",X]) ->
-    #make{actor = list_to_atom(X)}.
-
-decode_2([]) -> [];
-decode_2([$:|T]) -> decode_2(T);
-decode_2([$]|T]) -> decode_2(T);
-decode_2([A,B,C,D,E,F,$:|T]) ->
-    Taken = string:sub_string(T,1,list_to_integer([A,B,C,D,E,F])),
-    case Taken of
-	[$[|_] = LIST ->
-	    decode_2_list(LIST);
+decode_2([A,B,C,D,E,F,$:|R]) ->
+    case R of
+	[$[|T] ->
+	    [$],$:|T2] = lists:reverse(T),
+	    decode_in_list(lists:reverse(T2));
 	_ ->
-	    T3 = string:sub_string(T,1+list_to_integer([A,B,C,D,E,F])),
-	    [Taken | decode_2(T3)]
+	    R
     end.
 
-decode_2_list([$[,_,_,_,_,_,_,$:|T]) ->
-    decode_2(T).
+decode_in_list([A,B,C,D,E,F,$:|T]) ->
+    List_elements = list_to_integer([A,B,C,D,E,F]),
+    decode_elements(List_elements,T).
 
-    
+decode_elements(1, Data) ->
+    [ decode_2(Data) ];
+decode_elements(N, [A, B, C, D, E, F, $:|T]) ->
+    Taken = string:sub_string(T, 1,list_to_integer([A,B,C,D,E,F])),
+    Rest = string:sub_string(T, 2 + list_to_integer([A,B,C,D,E,F])),
+    [ decode_2([A,B,C,D,E,F,$:|Taken]) | decode_elements(N-1, Rest) ].
+
+to_command([]) -> [];
+to_command([ [_,"make","scriptTableActor",X] | T ]) ->
+    [ #make{actor = list_to_atom(X)} | to_command(T) ].
