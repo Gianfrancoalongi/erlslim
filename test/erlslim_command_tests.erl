481cde8d19303584b732ab519b7132fbc76f0719
Gianfrancoalongi <gianfranco.alongi@gmail.com>
2013-07-06 14:17:58 +0200

    Added call command test

--- test/erlslim_command_tests.erl
+++ test/erlslim_command_tests.erl
@@ -11,3 +11,15 @@ make_command_test() ->
     ?assertEqual([#result{id = "make_command_test", result = ok}],
 		 erlslim_command:execute(Commands)),
     ?assertEqual(lists,get(module)).
+
+call_command_test() ->
+    Commands = [#make{id = "call_command_test_1", actor = string},
+		#call{id = "call_command_test_2", 
+		      function = strip,
+		      args = [" a "]}
+	       ],
+    ?assertEqual([#result{id = "call_command_test_1", result = ok},
+		  #result{id = "call_command_test_2", result = "a"}
+		  ],
+		 erlslim_command:execute(Commands)).
+    
